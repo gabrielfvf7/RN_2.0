740ab30f54ed020ef58f2edb2f7e63bb6c81d50f
gabrielfvf7 <gabrielfvf@outlook.com>
2019-09-11 11:22:30 -0300

    counter screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -18,6 +18,10 @@ const HomeScreen = ({ navigation }) => {
       title="Go to image screen"
       onPress={() => navigation.navigate('Image')}
     />
+    <Button
+      title="Go to counter screen"
+      onPress={() => navigation.navigate('Counter')}
+    />
   </View>
   );
 };
