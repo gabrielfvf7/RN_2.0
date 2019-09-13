17971c19da7833486339747d6613c8ce5bba81e8
gabrielfvf7 <gabrielfvf@outlook.com>
2019-09-13 11:39:43 -0300

    searchbar style

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather name='search' style={styles.iconStyle}/>
+            <TextInput
+                autoCapitalize={false}
+                autoCapitalize='none' 
+                style={styles.inputStyle} 
+                placeholder='Search'
+                value={term}
+                onChangeText={onTermChange}
+                onEndEditing={onTermSubmit}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        marginTop: 15,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 40,
+        alignSelf: 'center',
+        marginHorizontal: 10
+    }
+});
+
+export default SearchBar;
