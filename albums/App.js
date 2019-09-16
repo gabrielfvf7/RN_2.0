94426a0b341ce9c65bf9f9150f148bc0d13fdf66
gabrielfvf7 <gabrielfvf@outlook.com>
2019-09-16 13:34:05 -0300

    expandindo a lista

---
+++ albums/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
+
+export default function App() {
+  return (
+    <Provider store={createStore(reducers)} >
+        <View style={{ flex: 1 }} >
+         <Header headerText='Tech Stack' />
+         <LibraryList />
+        </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ albums/components/LibraryList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { connect } from 'react-redux';
+import ListItem from './ListItem';
+
+class LibraryList extends Component {
+    renderItem(library) {
+        return <ListItem library={library} />;
+    }
+
+    render() {
+        return (
+            <FlatList 
+                data={this.props.libraries}
+                renderItem={this.renderItem}
+                keyExtractor={(library) => library.id}
+            />
+        );
+    }
+}
+
+// eslint-disable-next-line no-unused-vars
+const mapStateToProps = state => ({ libraries: state.libraries });
+
+export default connect(mapStateToProps)(LibraryList);
